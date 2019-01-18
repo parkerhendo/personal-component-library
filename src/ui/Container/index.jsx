45188c43864ec4e2e243ecee5614d0a1f18299a4
Parker Henderson <parker@parkerhendo.com>
2019-01-18 09:08:23 -0800

    added flex properties to Container

--- src/ui/Container/index.jsx
+++ src/ui/Container/index.jsx
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Container = styled.div`
   width: 80vw;
   max-width: 1170px;
   margin: 0 auto;
+  display: ${props => (props.flex ? 'flex' : 'block')};
+  justify-content: ${props => (props.flex && props.justifyContent)};
+  align-items: ${props => (props.flex && props.alignItems)};
 `;
+
+Container.propTypes = {
+  flex: PropTypes.bool,
+  justifyContent: PropTypes.string,
+  alignItems: PropTypes.string,
+};
 
 export default Container;
